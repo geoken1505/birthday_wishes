30d01fdf82cd1993ca3922ed23823fff3e3d4bdd
geoken1505 <96380463+geoken1505@users.noreply.github.com>
2026-01-11 12:47:18 +0530

    Update LoaderScreen.jsx

--- aiman-main/src/components/screens/LoaderScreen.jsx
+++ aiman-main/src/components/screens/LoaderScreen.jsx
@@ -43,7 +43,7 @@ export default function LoaderScreen({ onDone }) {
                 animate={{ opacity: [0.8, 1, 0.8] }}
                 transition={{ duration: 2, repeat: Infinity, ease: "easeInOut" }}
             >
-                Crafting your special moment... My Cheezy Bite :D           </motion.h1>
+                Crafting your special moment... My Beautiful Maharani          </motion.h1>
         </div>
     )
 }
